6f7e01d0feb69837c8f68635015ddef9f278ba7c
zakariaBAQASSE <zbaqasse51@gmail.com>
2023-03-01 15:31:41 +0100

    Feat: pass arguments from MessageFragment.kt to EncryptFragment.kt

--- app/src/main/java/com/example/secretmessage/MessageFragment.kt
+++ app/src/main/java/com/example/secretmessage/MessageFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.findNavController
 
 
@@ -18,8 +19,11 @@ class MessageFragment : Fragment() {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_message, container, false)
         val nextButton = view.findViewById<Button>(R.id.next)
+        val messageView = view.findViewById<EditText>(R.id.message)
         nextButton.setOnClickListener {
-            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
+            val message = messageView.text.toString()
+            val action = MessageFragmentDirections.actionMessageFragmentToEncryptFragment(message)
+            view.findNavController().navigate(action)
         }
         return view
     }
